243a0807d8eed3c589df79fe116cb1a498d74ccb
wsyoon <wsyoon@bitmango.com>
2019-10-31 13:29:43 +0900

    add generalized normalize.py

--- bin/normalize.py
+++ bin/normalize.py
@@ -2,40 +2,50 @@ import numpy as np
 import pandas as pd
 import os
 import sys
-import glob
-import matplotlib.pyplot as plt
+import pickle
 
-df = pd.read_csv(sys.argv[1])
+# assertions
+if (len(sys.argv) < 2) or (len(sys.argv) > 3):
+	print('Usg: python {} csvfile normalize_factors(optional)'.format(sys.argv[0]))
+	exit()
 
 plain_cols = ['spend', 'install']
 series_cols = ['retention', 'return', 'dau', 'mau', 'arpdau', 'act_sess', 'cpi', 'cpc']
 intervals = [1, 2, 3, 7]
+SCRIPTPATH = os.path.dirname(os.path.realpath(__file__))
 
+df = pd.read_csv(sys.argv[1])
+
+# load normalize factors
 normalize_factors = {}
+if len(sys.argv) == 3:
+	with open(sys.argv[2], 'rb') as f:
+		normalize_factors = pickle.load(f)
 
 # normalize plain cols
-for plain_col in plain_cols:
-	min_val = df[plain_col].min()
-	max_val = df[plain_col].max()
+for col in plain_cols + series_cols:
+	if col in plain_cols:
+		target_cols = [col]
+	else:
+		target_cols = ['{}_d{}'.format(col, i) for i in intervals]
 
-	normalize_factors[plain_col] = (min_val, max_val)
+	if len(sys.argv) == 3:
+		min_val = normalize_factors[col][0]
+		max_val = normalize_factors[col][1]
+	else:
+		min_val = df[target_cols].values.min()
+		max_val = df[target_cols].values.max()
+		normalize_factors[col] = (min_val, max_val)
 
-	df[plain_col] = df[plain_col].apply(
-		lambda x: 2 * ((x - min_val) / (max_val - min_val)) - 1.0)  # -1 ~ 1
-
-# normalize series cols
-for series_col in series_cols:
-	cols = ['{}_d{}'.format(series_col, i) for i in intervals]
-	min_val = df[cols].values.min()
-	max_val = df[cols].values.max()
-
-	normalize_factors[series_col] = (min_val, max_val)
-
-	for col in cols:
+	for col in target_cols:
 		df[col] = df[col].apply(
-			lambda x: 2 * ((x - min_val) / (max_val - min_val)) - 1.0)
+			lambda x: 2 * ((x - min_val) / (max_val - min_val)) - 1.0)  # -1 ~ 1
 
-print(normalize_factors, file=sys.stderr)
+# write normalize factors
+if len(sys.argv) == 2:  # if factors not given
+	with open('{}/normalize_factors.pkl'.format(SCRIPTPATH), 'wb') as f:
+		pickle.dump(normalize_factors, f)
+
 df.to_csv(sys.stdout, index=False)
 
 
